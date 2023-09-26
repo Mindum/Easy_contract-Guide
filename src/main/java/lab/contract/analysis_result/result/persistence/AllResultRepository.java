3b1973666a072a2855cbb41c075e505131396dd2
huiseonk <rhkr050800@gamil.com>
2023-09-26 17:09:36 +0900

    All Result Entity, Service

---
+++ src/main/java/lab/contract/analysis_result/result/persistence/AllResultRepository.java
@@ -0,0 +1,9 @@
+package lab.contract.analysis_result.result.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AllResultRepository extends JpaRepository<AllResult,Long> {
+
+}
---
+++ src/main/java/lab/contract/analysis_result/result/service/AllResultService.java
@@ -0,0 +1,26 @@
+package lab.contract.analysis_result.result.service;
+
+import lab.contract.allcontract.contract.persistence.Contract;
+import lab.contract.allcontract.contract.persistence.ContractRepository;
+import lab.contract.analysis_result.result.persistence.AllResult;
+import lab.contract.analysis_result.result.persistence.AllResultRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AllResultService {
+    private final ContractRepository contractRepository;
+    private final AllResultRepository allResultRepository;
+
+    public Long saveAllResult (Long contractId) {
+        Contract contract = contractRepository.findById(contractId).orElseThrow(EntityNotFoundException::new);
+        AllResult allResult = AllResult.builder().contract(contract).build();
+        contract.setAllResult(allResult);
+        return allResultRepository.save(allResult).getId();
+    }
+}
